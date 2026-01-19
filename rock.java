4ea960acffac5757121ae30afe43218c3949fb08
vishva sivanandam <vishvavishva0404@gmail.com>
2026-01-19 12:24:21 +0530

    new

---
+++ rock.java
@@ -0,0 +1,13 @@
+
+public class rock {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+
+        System.out.println("The first number is: " + num1);
+        System.out.println("The second number is: " + num2);
+        System.out.println("The sum is: " + sum); // Output: The sum is: 15
+    }
+}
