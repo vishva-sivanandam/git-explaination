f133367b65d90cedc16f701c2c11adac27e3a0e8
vishva sivanandam <vishvavishva0404@gmail.com>
2026-01-19 16:14:11 +0530

    Update vishva.java
    fix:proble

--- vishva.java
+++ vishva.java
@@ -2,10 +2,6 @@
 public class vishva {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("hello vishvasivanandam.S");
-=======
-        System.out.println("hello vishvasivanandam.N");
->>>>>>> 1028cc390d056490d0ad37393b21fea4829a2f4d
+   System.out.println("hello vishvasivanandam.S");
     }
 }
