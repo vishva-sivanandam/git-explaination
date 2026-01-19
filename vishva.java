f4efe949e125e0fbae8f6f3493112b8f3e7a4d16
vishva sivanandam <vishvavishva0404@gmail.com>
2026-01-19 16:26:18 +0530

    message

--- vishva.java
+++ vishva.java
@@ -2,10 +2,12 @@
 public class vishva {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("hello vishvasivanandam.S");
-=======
-        System.out.println("hello vishvasivanandam.N");
->>>>>>> 1028cc390d056490d0ad37393b21fea4829a2f4d
-    }
-}
+         == == ==
+                = System.out.println("hello vishvasivanandam.N");
+         >>> >>> > 1028
+        cc390d056490d0ad37393b21fea4829a2f4d 
+        
+         }  
+                 
+           
